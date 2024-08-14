90885b1a339314504c09bae84fb87b5604426127
Leo Liao <104142897+LeoLiao12138@users.noreply.github.com>
2024-08-14 18:44:42 +0800

    Add files via upload

---
+++ Modbus_TCP_data_collector/Modbus_TCP_data_collector/Modbus_TCP_data_collector/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Modbus_TCP_data_collector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.buttonReadData = new System.Windows.Forms.Button();
+            this.buttonStopRead = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxaddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(31, 77);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(114, 33);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(56, 36);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(89, 22);
+            this.textBoxIP.TabIndex = 1;
+            this.textBoxIP.Text = "127.0.0.1";
+            this.textBoxIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(284, 36);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(493, 389);
+            this.plotView1.TabIndex = 2;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // buttonReadData
+            // 
+            this.buttonReadData.Location = new System.Drawing.Point(164, 77);
+            this.buttonReadData.Name = "buttonReadData";
+            this.buttonReadData.Size = new System.Drawing.Size(114, 33);
+            this.buttonReadData.TabIndex = 3;
+            this.buttonReadData.Text = "ReadData";
+            this.buttonReadData.UseVisualStyleBackColor = true;
+            this.buttonReadData.Click += new System.EventHandler(this.buttonReadData_Click);
+            // 
+            // buttonStopRead
+            // 
+            this.buttonStopRead.Location = new System.Drawing.Point(164, 134);
+            this.buttonStopRead.Name = "buttonStopRead";
+            this.buttonStopRead.Size = new System.Drawing.Size(114, 33);
+            this.buttonStopRead.TabIndex = 4;
+            this.buttonStopRead.Text = "StopRead";
+            this.buttonStopRead.UseVisualStyleBackColor = true;
+            this.buttonStopRead.Click += new System.EventHandler(this.buttonStopRead_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(31, 134);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(114, 33);
+            this.buttonDisconnect.TabIndex = 5;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Address:";
+            // 
+            // textBoxaddress
+            // 
+            this.textBoxaddress.Location = new System.Drawing.Point(228, 36);
+            this.textBoxaddress.Name = "textBoxaddress";
+            this.textBoxaddress.Size = new System.Drawing.Size(50, 22);
+            this.textBoxaddress.TabIndex = 8;
+            this.textBoxaddress.Text = "0";
+            this.textBoxaddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxaddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonStopRead);
+            this.Controls.Add(this.buttonReadData);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Button buttonReadData;
+        private System.Windows.Forms.Button buttonStopRead;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxaddress;
+    }
+}
+
---
+++ Modbus_TCP_data_collector/Modbus_TCP_data_collector/Modbus_TCP_data_collector/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using Modbus.Device; // NModbus4  
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace Modbus_TCP_data_collector
+{
+    public partial class Form1 : Form
+    {
+        private ModbusIpMaster master;
+        private TcpClient tcpClient;
+        private Thread readThread;
+        private bool isReading = false;
+        private static int[] myarray = new int[100];
+        private List<int> dataBuffer = new List<int>(myarray);
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePlot();
+            buttonDisconnect.Enabled = false;
+            buttonReadData.Enabled = false;
+            buttonStopRead.Enabled = false;
+        }
+
+        private void InitializePlot()
+        {
+            var model = new PlotModel { Title = "Modbus Data" };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Time"});
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Value" });
+            var lineSeries = new LineSeries();
+            model.Series.Add(lineSeries);
+            plotView1.Model = model;
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClient(textBoxIP.Text, 502);
+                master = ModbusIpMaster.CreateIp(tcpClient);
+                MessageBox.Show("Connected successfully!");
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+                buttonReadData.Enabled = true;
+                buttonStopRead.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}");
+            }
+        }
+
+
+        private void buttonReadData_Click(object sender, EventArgs e)
+        {
+            if (isReading) return;
+            isReading = true;
+            readThread = new Thread(ReadData);
+            readThread.Start();
+        }
+
+        private void ReadData()
+        {
+            while (isReading)
+            {
+                try
+                {
+                    ushort[] registers = master.ReadHoldingRegisters(Convert.ToUInt16(textBoxaddress.Text), 1);
+                    dataBuffer.Add(registers[0]);                
+                    if (dataBuffer.Count > 100) dataBuffer.RemoveAt(0);
+
+                    Invoke(new Action(() =>
+                    {
+                        var series = (LineSeries)plotView1.Model.Series[0];
+                        series.Points.Clear();
+                        DateTime my_time = DateTime.Now;
+                        for (int i = 0; i < dataBuffer.Count; i++)
+                        {
+                            series.Points.Add(new DataPoint(i, dataBuffer[i]));
+                        }
+                        plotView1.InvalidatePlot(true);
+                    }));
+
+                    Thread.Sleep(1000); // Read every second  
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error reading data: {ex.Message}");
+                    isReading = false;
+                }
+            }
+        }
+
+        private void buttonStopRead_Click(object sender, EventArgs e)
+        {
+            isReading = false;
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            Tcp_disconnect();
+
+            // 如果ModbusIpMaster有特定的断开连接方法，也应该调用它（尽管这通常不是必需的，因为关闭TcpClient就足够了）  
+            // 但请注意，NModbus4等库可能没有提供显式的断开连接方法
+        }
+
+        private void Tcp_disconnect()
+        {
+            if (tcpClient != null)
+            {
+                try
+                {
+                    tcpClient.Close(); // 或者使用 tcpClient.Dispose();  
+                    tcpClient = null; // 可选：将tcpClient设置为null，以便垃圾回收  
+                    master = null; // 可选：将ModbusIpMaster实例设置为null
+                    isReading = false;
+                    MessageBox.Show("连接已断开");
+                    buttonConnect.Enabled = true;
+                    buttonDisconnect.Enabled = false;
+                    buttonReadData.Enabled = false;
+                    buttonStopRead.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error disconnect: {ex.Message}");
+
+                }
+
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Tcp_disconnect();
+        }
+    }
+}
